e25b9690495fc001e0324ebb20f6cc4b8c318602
Pere Bestard <perekuera@gmail.com>
2023-12-14 08:00:43 +0000

    backend nestjs configure

--- wi-backend/src/app.module.ts
+++ wi-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      //database: '',
+      entities: [],
+      //synchronize: true,
+    }),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
