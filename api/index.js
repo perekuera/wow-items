4d1169d9aabd28f39721cf6db045e2c6aea25190
Pere Bestard <perekuera@gmail.com>
2023-12-16 20:36:27 +0100

    vuetify front

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import itemsRouter from "./routes/items.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 app.use(express.json());
 
@@ -13,5 +16,7 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(
+    `Server running on ${process.env.NODE_ENV} environment, port ${port}`
+  );
 });
