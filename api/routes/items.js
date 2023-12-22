b831ad6e3cf10d1fba6cf1651615117cb4d7fa77
Pere Bestard <perekuera@gmail.com>
2023-12-22 11:45:56 +0100

    more services

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,5 +1,14 @@
 import express from "express";
-import { getItems, getItemClasses } from "../dao/item.js";
+import {
+  getItems,
+  getItemClasses,
+  getItemBondingTypes,
+  getItemDamageTypes,
+  getItemInventoryTypes,
+  getItemMaterials,
+  getItemQualities,
+  getItemStatTypes,
+} from "../dao/item.js";
 
 const router = express.Router();
 
@@ -7,8 +16,32 @@ router.get("/", async (req, res) => {
   res.json(await getItems(req.query));
 });
 
-router.get("/item-classes", (req, res) => {
+router.get("/item-classes", (_req, res) => {
   res.json(getItemClasses());
+});
+
+router.get("/item-bonding-types", (_req, res) => {
+  res.json(getItemBondingTypes);
+});
+
+router.get("/item-damage-types", (_req, res) => {
+  res.json(getItemDamageTypes());
+});
+
+router.get("/item-inventory-types", (_req, res) => {
+  res.json(getItemInventoryTypes());
+});
+
+router.get("/item-materials", (_req, res) => {
+  res.json(getItemMaterials());
+});
+
+router.get("/item-qualities", (_req, res) => {
+  res.json(getItemQualities());
+});
+
+router.get("/item-stat-types", (_req, res) => {
+  res.json(getItemStatTypes());
 });
 
 export default router;
