2ec7de9df4d01ef404e104ae9ad1a74e814b8e82
Pere Bestard <perekuera@gmail.com>
2023-12-22 11:41:56 +0100

    new dao's

--- api/dao/item.js
+++ api/dao/item.js
@@ -38,45 +38,73 @@ const CHECKED_QUERY = [
 ];
 
 const getItems = async (params = {}) => {
-  try {
-    const locale = params.locale || "enEN";
-    let query = `
+  const locale = params.locale || "enEN";
+  let query = `
       SELECT    it.*, 
                 itl.name AS locale_name 
       FROM      acore_world.item_template it 
       LEFT JOIN acore_world.item_template_locale itl ON (entry = id AND locale = '${locale}')
     `;
-    params = Object.fromEntries(
-      Object.entries(params).filter(
-        ([key, _value]) => key && CHECKED_QUERY.includes(key.toLowerCase())
-      )
-    );
+  params = Object.fromEntries(
+    Object.entries(params).filter(
+      ([key, _value]) => key && CHECKED_QUERY.includes(key.toLowerCase())
+    )
+  );
 
-    const conditions = Object.keys(params)
-      .map((key) => {
-        if ("name" === key.toLowerCase()) {
-          return `lower(concat(it.name, '|', itl.name) LIKE '%' || ? || '%'`;
-        }
-        return params[key] !== undefined ? `${key.toLowerCase()} = ?` : null;
-      })
-      .filter((condition) => condition !== null)
-      .join(" AND ");
+  const conditions = Object.keys(params)
+    .map((key) => {
+      if ("name" === key.toLowerCase()) {
+        return `lower(concat(it.name, '|', itl.name) LIKE '%' || ? || '%'`;
+      }
+      return params[key] !== undefined ? `${key.toLowerCase()} = ?` : null;
+    })
+    .filter((condition) => condition !== null)
+    .join(" AND ");
 
-    if (conditions) {
-      query += ` WHERE ${conditions}`;
-    }
-
-    query += " LIMIT 1";
-    const [rows] = await pool.execute(query, Object.values(params));
-    return rows;
-  } catch (error) {
-    console.error("MI ERROR! ", error);
-    throw error;
+  if (conditions) {
+    query += ` WHERE ${conditions}`;
   }
+
+  query += " LIMIT 1";
+  const [rows] = await pool.execute(query, Object.values(params));
+  return rows;
 };
 
 const getItemClasses = () => {
   return require(path.join(__dirname, "static/item-classes.json"));
 };
 
-export { getItems, getItemClasses };
+const getItemBondingTypes = () => {
+  return require(path.join(__dirname, "static/item-bonding-types.json"));
+};
+
+const getItemDamageTypes = () => {
+  return require(path.join(__dirname, "static/item-damage-types.json"));
+};
+
+const getItemInventoryTypes = () => {
+  return require(path.join(__dirname, "static/item-inventory-types.json"));
+};
+
+const getItemMaterials = () => {
+  return require(path.join(__dirname, "static/item-materials.json"));
+};
+
+const getItemQualities = () => {
+  return require(path.join(__dirname, "static/item-qualities.json"));
+};
+
+const getItemStatTypes = () => {
+  return require(path.join(__dirname, "static/item-stat-types.json"));
+};
+
+export {
+  getItems,
+  getItemClasses,
+  getItemBondingTypes,
+  getItemDamageTypes,
+  getItemInventoryTypes,
+  getItemMaterials,
+  getItemQualities,
+  getItemStatTypes,
+};
