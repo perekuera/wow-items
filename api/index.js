64c7e7af5c20885a7c9e00a2c6019deb5638b418
Pere Bestard <perekuera@gmail.com>
2024-01-07 11:43:48 +0100

    configs

--- api/index.js
+++ api/index.js
@@ -30,22 +30,26 @@ app.use("/characters", charactersRouter);
 app.use("/commands", commandsRouter);
 
 app.use((err, req, res, _next) => {
-  let status = 500;
-  if (err.message.startsWith("Authorization")) {
-    status = 401;
-  }
-  res
-    .status(status)
-    .json({ request: req.path, method: req.method, message: err.message });
+    let status = 500;
+    if (err.message.startsWith("Authorization")) {
+        status = 401;
+    }
+    res.status(status).json({
+        request: req.path,
+        method: req.method,
+        message: err.message,
+    });
 });
 
 app.get("/locales", (req, res) => {
-  const locales = require(path.join(__dirname, "dao/static/locales.json"));
-  res.json(locales);
+    const locales = require(path.join(__dirname, "dao/static/locales.json"));
+    res.json(locales);
 });
 
 app.listen(port, () => {
-  console.log(
-    `Server running on ${process.env.NODE_ENV} environment, port ${port}`
-  );
+    console.log(
+        `Server running on ${process.env.NODE_ENV} environment, port ${port}`
+    );
+    console.log(`AC_DB_HOST is ${process.env.AC_DB_HOST}`);
+    console.log(`AC_SOAP_HOST is ${process.env.AC_SOAP_HOST}`);
 });
